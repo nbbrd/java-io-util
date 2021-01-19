191eeb8a780fadde8c74b5adb512dc7c5633e9cc
Philippe Charles <philippe.charles@nbb.be>
2021-01-19 15:42:26 +0100

    Add tests on SystemProperties

--- java-io-base/src/test/java/nbbrd/io/sys/SystemPropertiesTest.java
+++ java-io-base/src/test/java/nbbrd/io/sys/SystemPropertiesTest.java
@@ -3,18 +3,100 @@ package nbbrd.io.sys;
 import com.google.common.base.StandardSystemProperty;
 import org.junit.Test;
 
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 public class SystemPropertiesTest {
 
     @Test
-    public void test() {
+    public void testFactories() {
+        assertThatNullPointerException()
+                .isThrownBy(() -> SystemProperties.of(null, FileSystems.getDefault()));
+
+        assertThatNullPointerException()
+                .isThrownBy(() -> SystemProperties.of(System.getProperties(), null));
+
+        assertThat(SystemProperties.DEFAULT)
+                .isNotNull();
+    }
+
+    @Test
+    public void testGetUserHome() {
+        Properties p = new Properties();
+        SystemProperties x = SystemProperties.of(p, FileSystems.getDefault());
+
+        assertThat(x.getUserHome())
+                .isNull();
+
+        p.put(SystemProperties.USER_HOME, "C:\\Temp");
+        assertThat(x.getUserHome())
+                .isEqualByComparingTo(Paths.get("C:\\Temp"));
+    }
+
+    @Test
+    public void testGetUserDir() {
+        Properties p = new Properties();
+        SystemProperties x = SystemProperties.of(p, FileSystems.getDefault());
+
+        assertThat(x.getUserDir())
+                .isNull();
+
+        p.put(SystemProperties.USER_DIR, "C:\\Temp");
+        assertThat(x.getUserDir())
+                .isEqualByComparingTo(Paths.get("C:\\Temp"));
+    }
+
+    @Test
+    public void testGetJavaClassPath() {
+        Properties p = new Properties();
+        SystemProperties x = SystemProperties.of(p, FileSystems.getDefault());
+
+        assertThat(x.getJavaClassPath())
+                .isEmpty();
+
+        p.put(SystemProperties.PATH_SEPARATOR, ";");
+        assertThat(x.getJavaClassPath())
+                .isEmpty();
+
+        p.put(SystemProperties.JAVA_CLASS_PATH, "C:\\Temp\\x.jar");
+        assertThat(x.getJavaClassPath())
+                .containsExactly(Paths.get("C:\\Temp\\x.jar"));
+
+        p.put(SystemProperties.JAVA_CLASS_PATH, "C:\\Temp\\x.jar;C:\\Temp\\y.jar");
+        assertThat(x.getJavaClassPath())
+                .containsExactly(
+                        Paths.get("C:\\Temp\\x.jar"),
+                        Paths.get("C:\\Temp\\y.jar")
+                );
+    }
+
+    @Test
+    public void testGetPathSeparator() {
+        Properties p = new Properties();
+        SystemProperties x = SystemProperties.of(p, FileSystems.getDefault());
+
+        assertThat(x.getPathSeparator()).isNull();
+
+        p.put(SystemProperties.PATH_SEPARATOR, "");
+        assertThat(x.getPathSeparator()).isNull();
+
+        p.put(SystemProperties.PATH_SEPARATOR, "; ");
+        assertThat(x.getPathSeparator()).isNull();
+
+        p.put(SystemProperties.PATH_SEPARATOR, ";");
+        assertThat(x.getPathSeparator()).isEqualTo(';');
+    }
+
+    @Test
+    public void testDEFAULT() {
         SystemProperties p = SystemProperties.DEFAULT;
 
         assertThat(p.getJavaVersion()).isEqualTo(StandardSystemProperty.JAVA_VERSION.value());
